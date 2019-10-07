80dd0c745f7aa5a1bf439dab9f326f561eb01e22
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2019-10-07 13:44:07 +0300

    chore: fix delaration

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,14 @@
-declare module 'windows-notification-state' {
-  export function getNotificationState(): QUERY_USER_NOTIFICATION_STATE | 'UNKNOWN_ERROR';
+declare module "@aabuhijleh/windows-notification-state" {
+  export function getNotificationState():
+    | QUERY_USER_NOTIFICATION_STATE
+    | "UNKNOWN_ERROR";
   export function shQueryUserNotificationState(): number;
-  export type QUERY_USER_NOTIFICATION_STATE = 'QUNS_NOT_PRESENT' | 'QUNS_BUSY' | 'QUNS_RUNNING_D3D_FULL_SCREEN' | 'QUNS_PRESENTATION_MODE' | 'QUNS_ACCEPTS_NOTIFICATIONS' | 'QUNS_QUIET_TIME' | 'QUNS_APP';
+  export type QUERY_USER_NOTIFICATION_STATE =
+    | "QUNS_NOT_PRESENT"
+    | "QUNS_BUSY"
+    | "QUNS_RUNNING_D3D_FULL_SCREEN"
+    | "QUNS_PRESENTATION_MODE"
+    | "QUNS_ACCEPTS_NOTIFICATIONS"
+    | "QUNS_QUIET_TIME"
+    | "QUNS_APP";
 }
